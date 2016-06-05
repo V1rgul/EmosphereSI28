4a9656207140faf079ac6a510981cd2b02094d3f
v1rgul <v1rgul.42@gmail.com>
2016-06-05 20:46:34 +0200

    remove parasitic vibration

--- SI28-Arduino/outputs/vibration.h
+++ SI28-Arduino/outputs/vibration.h
@@ -3,6 +3,7 @@
 
 #define VIBRATION_PIN 12
 #define VIBRATION_MAX 1.0F
+#define VIBRATION_THERSHOLD 0.03F
 
 class Vibration {
 protected:
@@ -16,7 +17,7 @@ public:
 		pinMode(VIBRATION_PIN, OUTPUT);
 	}
 	void setValue(float strengh, float lfoGain, float lfoWaveform){
-		float v = strengh * lfo.computeForm(lfoWaveform, lfoGain) * VIBRATION_MAX;
+		float v = max(0,strengh-VIBRATION_THERSHOLD) * lfo.computeForm(lfoWaveform, lfoGain) * VIBRATION_MAX;
 		analogWrite(VIBRATION_PIN, v*1024);
 	}
 };
@@ -36,7 +37,7 @@ public:
 		SoftPWMBegin(SOFTPWM_NORMAL);
 	}
 	void setValue(float strengh, float lfoGain, float lfoWaveform){
-		float v = strengh * lfo.computeForm(lfoWaveform, lfoGain) * VIBRATION_MAX;
+		float v = max(0,strengh-VIBRATION_THERSHOLD) * lfo.computeForm(lfoWaveform, lfoGain) * VIBRATION_MAX;
 		SoftPWMSet(VIBRATION_PIN, v*255);
 	}
 };
